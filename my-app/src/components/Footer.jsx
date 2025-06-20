f2f3401717dcb48e39c95dff3a5135bab59bc42d
MahlatsePhetla <phetlamahlatse09@gmail.com>
2025-06-20 11:48:21 +0200

    Updates

--- my-app/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() {
-  return (
-    <footer className="text-center p-4 text-sm text-gray-500 dark:text-gray-400">
-      &copy; {new Date().getFullYear()} Task Manager App
-    </footer>
-  );
-}
